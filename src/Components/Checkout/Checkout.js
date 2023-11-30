f9ed09cebd09e7f1da371edb3d2938b282613f72
upendrapanchali <upendrapanchali83@gmail.com>
2023-11-30 19:18:59 +0530

    sucessfully added the data

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -15,6 +15,14 @@ function Checkout(){
     const navigate= useNavigate();
     const [totalprice,settotalprice] = useState(0);
     const [radiooption,setradiooption]=useState('option1');
+    const [paymentgate,setpaymentgate]=useState('gateway1');
+    const [carddet,setcarddet] = useState({
+        name:'',
+        number:'',
+        validm:'',
+        validd:'',
+        cvv:''
+    })
     
     const batch = writeBatch(db);
 
@@ -56,7 +64,7 @@ function Checkout(){
             return {
                 orderid:uuidv4(),
                 items:sharedvalue.mycart,
-                totalprice:radiooption==='option1'?totalprice+5:totalprice,
+                totalprice:paymentgate==='gateway1'?radiooption==='option1'?totalprice+5:totalprice :0,
                 address:addressdetail,
                 uid:sharedvalue.isauthed.uid,
                 status:"pending"
@@ -108,6 +116,18 @@ function Checkout(){
     catch(e){
         console.error('you got an order',e);
     }
+    }
+
+    async function cardpayfun(){
+        try{
+            if(carddet.name!=='' && carddet.number!=='' && carddet.cvv!=='' && carddet.validd!=='' && carddet.validm!==''){
+                await handleplaceorder()
+            }else{
+                alert('please fill the card details perfectly');
+            }
+        }catch(e){
+            alert('you got an error while doing the proceed to pay')
+        }
     }
 
     return(
@@ -202,8 +222,60 @@ function Checkout(){
                             <p>Total:</p>
                             <p>${radiooption==='option1'?Number(totalprice)+5:Number(totalprice)}</p>
                         </div>
-                        <p>Cash on delivery Only</p>
-                        <button onClick={()=>handleplaceorder()}>Place Order</button>
+                        <div>
+                        <div className='checkout-bill-con-each-two-divs-one'>
+                            <p>Payment Method:</p>
+                            <label>
+                            <input type='radio' value='gateway1' checked={paymentgate==='gateway1'} onChange={()=>setpaymentgate('gateway1')}/>
+                            Cash On Delivery
+                            </label>
+                            <label>
+                            <input type='radio' value='gateway2' checked={paymentgate==='gateway2'} onChange={()=>setpaymentgate('gateway2')}/>
+                            Pay With Card
+                            </label>
+                            {paymentgate==='gateway2' && 
+                            <div className='checkout-card-details'>
+                                <h1>Card Details:</h1>
+                                <div>
+                                    <label>Card Number:</label>
+                                    <input placeholder='Card Number'type='number' onChange={(e)=>setcarddet(prev=>({
+                                        ...prev,
+                                        number:e.target.value
+                                    }))}/>
+                                </div>
+                                <div>
+                                    <label>Name:</label>
+                                    <input placeholder='Name' type='text' onChange={(e)=>setcarddet(prev=>({
+                                        ...prev,
+                                        name:e.target.value
+                                    }))}/>
+                                </div>
+                                <div>
+                                    <div>
+                                    <label>valid To</label>
+                                    <div>
+                                    <input type='number' placeholder="month" onChange={(e)=>setcarddet(prev=>({
+                                        ...prev,
+                                        validm:e.target.value
+                                    }))}/>/<input type='number' placeholder='day' onChange={(e)=>setcarddet(prev=>({
+                                        ...prev,
+                                        validd:e.target.value
+                                    }))}/>
+                                    </div>
+                                    </div>
+                                    <div>
+                                    <label>cvv</label>
+                                    <input type='number' placeholder='CVV' onChange={(e)=>setcarddet(prev=>({
+                                        ...prev,
+                                        cvv:e.target.value
+                                    }))}/>
+                                    </div>
+                                </div>
+                                <button onClick={()=>cardpayfun()}>proceed to pay</button>
+                            </div>}
+                            </div>
+                        </div>
+                        {paymentgate==='gateway1' && <button onClick={()=>handleplaceorder()}>Place Order</button>}
                     </div>
 
                 </div>
